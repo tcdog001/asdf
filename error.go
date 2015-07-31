53696d052a4491a201f682afcdfd0a095a2e1b96
ljf10000 <l_j_f@163.com>
2015-07-31 16:39:50 +0800

    interface

--- error.go
+++ error.go
@@ -12,7 +12,8 @@ var ErrNoMatch		= errors.New("no match")
 var ErrNoSpace 		= errors.New("no space")
 
 var ErrBadObj 		= errors.New("bad obj")
-
 var ErrNilObj 		= errors.New("nil obj")
 
-var ErrTooShortBuffer = errors.New("too short buffer")
+var ErrTooShortBuffer 	= errors.New("too short buffer")
+var ErrBadPktLen 		= errors.New("invalid packet length")
+var ErrPktLenNoMatchBufferLen 	= errors.New("packet length not match buffer length")
