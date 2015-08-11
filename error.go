9825452bf2b571d3bce6d1f80f08731b8855d8e0
ljf10000 <l_j_f@163.com>
2015-08-11 19:01:48 +0800

    .

--- error.go
+++ error.go
@@ -14,6 +14,8 @@ var ErrNoSpace = errors.New("no space")
 var ErrBadObj = errors.New("bad obj")
 var ErrNilObj = errors.New("nil obj")
 var ErrBadIdx = errors.New("bad idx")
+var ErrBadIntf= errors.New("bad interface")
+var ErrBadType= errors.New("bad type")
 var ErrNilBuffer = errors.New("nil buffer")
 
 var ErrTooShortBuffer = errors.New("too short buffer")
--- timer.go
+++ timer.go
@@ -5,69 +5,66 @@ import (
 )
 
 const (
-	TM_BIT 		= 8				// 8
-	TM_MASK 	= 1<<TM_BIT - 1	// 255
-	TM_SLOT 	= TM_MASK + 1	// 256
-	TM_RING 	= 4*8/TM_BIT	// 4
-	TM_RINGMAX 	= TM_RING - 1	// 3
-	
-	TM_PENDING 	uint8 = 1
-	TM_CYCLE 	uint8 = 2
+	tm_BIT 		uint = 8			// 8
+	tm_MASK 	uint = 1<<tm_BIT - 1// 255
+	tm_SLOT 	uint = tm_MASK + 1	// 256
+	tm_RING 	uint = 4*8/tm_BIT	// 4
+	tm_RINGMAX 	uint = tm_RING - 1	// 3
+
+	tm_PENDING 	uint = 1
+	tm_CYCLE 	uint = 2
 )
 
-type TimerCallback func() (bool, error)
+type TimerCallback func(entry interface{}) (bool, error)
 
-type Timer struct {
+type ITimer interface {
+	GetTimer(Type uint) interface{}
+}
+
+type tmNode struct {
 	cb 		TimerCallback
-	flag	uint8
-	idx 	uint8
-	slot 	uint16
-	expires	uint32
+	
+	flag	uint
+	slot 	uint	// ring slot
+	expires	uint
 	create	uint64
 	
+	ring 	*tmRing
 	node *list.Element
 }
 
-func (me *Timer) Idx() uint32 {
-	return uint32(me.idx)
+func (me *tmNode) IsCycle() bool {
+	return tm_CYCLE==(tm_CYCLE & me.flag)
 }
 
-func (me *Timer) Slot() uint32 {
-	return uint32(me.slot)
+func (me *tmNode) IsPending() bool {
+	return tm_PENDING==(tm_PENDING & me.flag)
 }
 
-func (me *Timer) Flag() uint32 {
-	return uint32(me.flag)
+func (me *tmNode) Clock() *Clock {
+	return me.ring.clock
 }
 
-func (me *Timer) IsCycle() bool {
-	return TM_CYCLE==(TM_CYCLE & me.flag)
-}
-
-func (me *Timer) IsPending() bool {
-	return TM_PENDING==(TM_PENDING & me.flag)
-}
-
-func (me *Timer) left() uint32 {
+func (me *tmNode) Left() uint {
 	timeout := me.create + uint64(me.expires)
 	
-	if timeout > clock.ticks {
-		return uint32(timeout - clock.ticks)
+	if timeout > me.Clock().ticks {
+		return uint(timeout - me.Clock().ticks)
 	}
 	
 	return 0
 }
 
-func (me *Timer) findRing() (uint32 /* slot */, *tmRing) {
-	left := me.left()
+func (me *tmNode) FindRing() (uint, *tmRing) {
+	left := me.Left()
 	
-	offset := uint32(0)
-	idx := uint32(TM_RINGMAX)
+	offset := uint(0)
+	idx := tm_RINGMAX
 	
 	for ;idx>0; idx-- {
 		offset = left
-		offset <<= TM_BIT*(TM_RINGMAX-idx)
-		offset >>= TM_BIT*TM_RINGMAX
+		offset <<= tm_BIT*(tm_RINGMAX-idx)
+		offset >>= tm_BIT*tm_RINGMAX
 		
 		if offset > 0 {
 			break
@@ -78,90 +75,157 @@ func (me *Timer) findRing() (uint32 /* slot */, *tmRing) {
 		offset = left
 	}
 	
-	r := ringX(idx)
+	r := me.Clock().ringX(idx)
 
-	return (r.current + offset) & TM_MASK, r
+	return (r.current + offset) & tm_MASK, r
 }
 
-func (me *Timer) Insert() {
-	slot, r := me.findRing()
+func (me *tmNode) Insert() error {
+	slot, r := me.FindRing()
 	
-	me.node = r.Slot(slot).PushFront(me)
-	me.idx 	= uint8(r.idx)
-	me.slot = uint16(slot)
-	me.flag |= TM_PENDING
+	me.node = r.List(slot).PushFront(me)
+	me.slot = slot
+	me.flag |= tm_PENDING
 	
-	r.count++
-	clock.count++
-	clock.inserted++
-	
-	if clock.ticks > 0 {
-		// test ???
-	}
-}
-
-func (me *Timer) Remove() {
-	r := ringX(me.Idx())
-	
-	if clock.ticks > 0 {
+	if me.Clock().ticks > 0 {
 		// test ???
 	}
 	
-	r.Slot(me.Slot()).Remove(me.node)
+	return nil
+}
+
+func (me *tmNode) Remove() error {
+	r := me.ring
+	
+	if me.Clock().ticks > 0 {
+		// test ???
+	}
+	
+	r.List(me.slot).Remove(me.node)
 	me.node = nil
-	me.flag &= ^TM_PENDING
+	me.flag &= ^tm_PENDING
 	
-	r.count--
-	clock.count--
-	clock.removed++
+	return nil
 }
 
 type tmRing struct {
-	slot [TM_SLOT]list.List
+	list [tm_SLOT]list.List
 	
-	current uint32
-	count 	uint32
-	idx 	uint32
+	current uint
+	idx 	uint // ring index
+	
+	clock 	*Clock
 }
 
-func (me *tmRing) Slot(idx uint32) *list.List {
-	return &me.slot[idx]
+func (me *tmRing) List(idx uint) *list.List {
+	return &me.list[idx]
 }
 
-type tmClock struct {
+type Clock struct {
 	init 	bool
 	ticks 	uint64
 	
-	triggered_safe		uint64
-	triggered_unsafe	uint64
-	triggered_error		uint64
-	triggered_ok		uint64
-	inserted			uint64
-	removed				uint64
+	ring 	[tm_RING]tmRing
+	unit 	uint
+}
+
+func (me *Clock) ringX(idx uint) *tmRing {
+	return &me.ring[idx]
+}
+
+func (me *Clock) ring0() *tmRing {
+	return me.ringX(0)
+}
+
+func (me *Clock) ringMax() *tmRing {
+	return me.ringX(tm_RINGMAX)
+}
+
+func (me *Clock) Ticks() uint64 {
+	return me.ticks
+}
+
+func (me *Clock) SetUnit(unit uint/* ms */) {
+	me.unit = unit
+}
+
+func (me *Clock) GetUnit() uint {
+	return me.unit
+}
+
+func (me *Clock) Trigger(times uint) error {
+	return nil
+}
+
+func (me *Clock) Insert(
+		entry interface{}, 
+		after uint, 
+		cb TimerCallback, 
+		Cycle bool) (interface{}, error) {
+	if _, ok := entry.(ITimer); !ok {
+		return nil, ErrBadIntf
+	}
+
+	flag := uint(0)
+	if Cycle {
+		flag = tm_CYCLE
+	}
 	
-	ring 	[TM_RING]tmRing
-	count 	uint32
-	unit 	uint32
+	t := &tmNode{
+		cb:cb,
+		create:me.ticks,
+		expires:after,
+		flag:flag,
+	}
+	
+	if err := t.Insert(); nil!=err {
+		return nil, err
+	}
+	
+	return t, nil
 }
 
-var clock = &tmClock{}
-
-func ring0() *tmRing {
-	return &clock.ring[0]
+func (me *Clock) Remove(entry interface{}, Type uint) error {
+	t, err := tmNodeGet(entry, Type)
+	if nil!=err {
+		return err
+	}
+	
+	return t.Remove()
 }
 
-func ringX(idx uint32) *tmRing {
-	return &clock.ring[idx]
+func tmNodeGet(entry interface{}, Type uint) (*tmNode, error) {
+	var iEntry ITimer
+	var ok bool
+	var t *tmNode
+	
+	iEntry, ok = entry.(ITimer)
+	if !ok {
+		return nil, ErrBadIntf
+	}
+	
+	t, ok = iEntry.GetTimer(Type).(*tmNode)
+	if !ok {
+		return nil, ErrBadType
+	}
+	
+	return t, nil
 }
 
-func ringMax() *tmRing {
-	return &clock.ring[TM_RINGMAX]
-}
-
-func TmInit() {
-
-}
-
-func TmFini() {
-
+func TmClock() *Clock {
+	c := &Clock{}
+	
+	for i:=uint(0); i<tm_RING; i++ {
+		r := c.ringX(i)
+		
+		r.idx = i
+		r.clock = c
+		r.current = 0
+		
+		for j:=uint(0); j<tm_SLOT; j++ {
+			r.List(j).Init()
+		}
+	}
+	
+	return c
 }
